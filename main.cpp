6a0ddf9516733786abe3351534d7b89c9fdc9539
Neil Johnson <mr.neil.j@gmail.com>
2017-02-18 01:08:22 -0800

    Trying to remedy the issue as to where the application is dumping.

--- main.cpp
+++ main.cpp
@@ -24,10 +24,10 @@ int main() {
     int testArrayD[10] = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5,};
 
     // Run the test
-    findMode(testArrayA, 10);
-    findMode(testArrayB, 10);
-    findMode(testArrayC, 10);
-    findMode(testArrayD, 10);
+    std::cout << "The Mode for Array A is: " << findMode(testArrayA, 10);
+    std::cout << "The Mode for Array B is: " << findMode(testArrayB, 10);
+    std::cout << "The Mode for Array C is: " << findMode(testArrayC, 10);
+    std::cout << "The Mode for Array D is: " << findMode(testArrayD, 10);
 
     return 0;
 
@@ -35,11 +35,10 @@ int main() {
 
 int findMode(int inputArray[], int arraySize)
 {
-    int maxFreq     = 0;
+    int maxFreq = 0;
+    std::vector<int> modeVec;
 
-
-
-    // Loop through entire array
+    // Loop through entire array and find the highest frequency
     for (int i = 0; i < arraySize; i++)
     {
         int currentFreq = 0;
@@ -61,6 +60,57 @@ int findMode(int inputArray[], int arraySize)
         if (currentFreq > maxFreq)
         {
             maxFreq = currentFreq;
+        }
+    }
+
+    // Loop through entire array again and compare max frequency to other number frequencies
+    for (int i = 0; i < arraySize; i++)
+    {
+        int currentFreq = 0;
+        int currentVal;
+
+        // Set the current value we are looking for
+        currentVal = inputArray[i];
+
+        // Loop through rest of remain loop
+        for (int j = i; j < arraySize; j++)
+        {
+            // Check to see if any other numbers match
+            if (inputArray[j] == currentVal)
+            {
+                // Add to current frequency counter
+                currentFreq++;
+            }
+        }
+        if (currentFreq == maxFreq)
+        {
+            if (!modeVec.empty())
+            {
+                // Check to make sure the value isn't already stored.
+                bool valExists = false;
+                int k = 0;
+                while (!valExists)
+                {
+                    if (inputArray[i] == modeVec.at(k))
+                    {
+                        valExists = true;
+                    }
+                    else
+                    {
+                        if (k == i) {
+                            modeVec.push_back(inputArray[i]);
+                        } else {
+                            k++;
+                        }
+                    }
+                }
+            }
+            // Add first value to the vector
+            else
+            {
+                modeVec.push_back(inputArray[i]);
+            }
+
         }
     }
 }
