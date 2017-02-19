c146406e2a6bc67b5a7a085a2213d5bce70b58da
neiljohnson <mr.neil.j@gmail.com>
2017-02-19 14:00:46 -0800

    Added an array and vector set to ensure simpler functionality without having to loop through the array too many times

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+/**************************************************************
+ * Author: Neil Johnson
+ *
+ * Date: 2.17.2017
+ *
+ * Description: This file hosts a function known as "findMode"
+ * that allows the user to provide an array and the size of
+ * that array and the function will return a vector holding
+ * the mode(s} that the array holds.  In addition to this it
+ * will sort the values from lowest to highest value and will
+ * only show a value once (no repetition).
+ *
+ * There is a secondary function called printMode that allows
+ * the user to print the results of the function findMode to
+ * the console. The user must provide the initial array and
+ * size of the array, just like they did in the findMode
+ * function.
+**************************************************************/
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -7,9 +25,9 @@ std::vector<int> findMode(int array[], int arraySize);
 void printMode(int inputArray[], int arraySize);
 
 int main() {
-    /**************************
-     * TESTS THAT NEED TO PASS
-     **************************/
+    /***************************
+     * TESTS THAT NEED TO PASS *
+     ***************************/
 
     // All unique values (one of each)
     int testArrayA[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,};
@@ -23,8 +41,7 @@ int main() {
     // All double values
     int testArrayD[10] = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5,};
 
-    // Run the test
-
+    // Run the tests using the "printMode" function
     std::cout << "{1, 2, 3, 4, 5, 6, 7, 8, 9, 10,}" << " Mode = ";
     printMode(testArrayA, 10);
 
@@ -37,10 +54,6 @@ int main() {
     std::cout << "{1, 1, 2, 2, 3, 3, 4, 4, 5, 5,}" << "  Mode = ";
     printMode(testArrayD, 10);
 
-
-
-
-
     return 0;
 
 }
@@ -48,6 +61,12 @@ int main() {
 std::vector<int> findMode(int inputArray[], int arraySize)
 {
     int maxFreq = 0;
+
+    // Temporary array to hold value and frequency
+    int tempArray[1][1];
+
+    // Temporary vector to hold the array values of each frequency count
+    std::vector<int> tempVec;
     std::vector<int> modeVec;
 
     // Loop through entire array and find the highest frequency
@@ -56,24 +75,32 @@ std::vector<int> findMode(int inputArray[], int arraySize)
         int currentFreq = 0;
         int currentVal;
 
-        // Set the current value we are looking for
+        // Set the current value we are looking to count in the array
         currentVal = inputArray[i];
 
-        // Loop through rest of remain loop
+        // Loop through rest of remaining loop
         for (int j = i; j < arraySize; j++)
         {
-            // Check to see if any other numbers match
+            // Check to see if any other numbers match the current value
+            // that we are looking for.
             if (inputArray[j] == currentVal)
             {
                 // Add to current frequency counter
                 currentFreq++;
             }
         }
+        // If the counter is higher than the highest counted number, set it to maxFreq
         if (currentFreq > maxFreq)
         {
             maxFreq = currentFreq;
+
         }
     }
+
+
+
+
+    /*
 
     // Loop through entire array again and compare max frequency to other number frequencies
     for (int i = 0; i < arraySize; i++)
@@ -115,7 +142,6 @@ std::vector<int> findMode(int inputArray[], int arraySize)
                     else
                     {
                         k++;
-
                     }
                 }
             }
@@ -129,13 +155,11 @@ std::vector<int> findMode(int inputArray[], int arraySize)
     }
     std::sort(modeVec.begin(), modeVec.end());
     return modeVec;
+ */
+
 }
 
 
-/******************************************************************
- *
- *
- */
 void printMode(int inputArray[], int arraySize)
 {
     std::vector<int> modeOutput = findMode(inputArray, arraySize);
